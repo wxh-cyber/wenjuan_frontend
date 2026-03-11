62db3b3642a6d6886488c91f8344ea3098df3966
风雪心 <1097071510@qq.com>
2026-03-11 21:39:39 +0800

    feat: 增加了保存和发布按钮的逻辑

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,12 +1,118 @@
-import { FC } from "react";
-import { Button,Typography ,Space} from 'antd';
-import { LeftOutlined } from '@ant-design/icons';
-import { useNavigate } from "react-router-dom";
+import { FC, useState, ChangeEvent, useEffect } from "react";
+import { Button, Typography, Space, Input, message } from 'antd';
+import { LeftOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons';
+import { useNavigate, useParams } from "react-router-dom";
+import useGetPageInfo from '../../../hooks/useGetPageInfo';
+import useGetComponentsInfo from "../../../hooks/useGetComponentsInfo";
+import { useDispatch } from 'react-redux';
+import { changePageTitle } from '../../../store/pageInfoReducer';
+import { updateQuestionService } from "../../../services/question";
+import { useRequest, useKeyPress, useDebounceEffect } from "ahooks";
 import styles from './EditHeader.module.scss';
 import EditToolBar from "./EditToolBar";
 
 const { Title } = Typography;
 
+//显示和修改标题
+const TitleElem: FC = () => {
+    const { title } = useGetPageInfo();
+    const dispatch = useDispatch();
+
+    const [editState, setEditState] = useState(false);
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const newTitle = event.target.value.trim();
+        if (!newTitle) return;
+        dispatch(changePageTitle(newTitle));
+    }
+
+    if (editState) {
+        return <Input
+            value={title}
+            onChange={handleChange}
+            onPressEnter={() => setEditState(false)}
+            onBlur={() => setEditState(false)}
+        />
+    }
+
+    return (
+        <Space>
+            <Title>{title}</Title>
+            <Button
+                onClick={() => setEditState(true)}
+                type="text"
+                icon={<EditOutlined />}></Button>
+        </Space>
+    )
+}
+
+//保存按钮
+const SaveButton: FC = () => {
+    //pageInfo ComponentList
+    const { id } = useParams();
+    const { componentList = [] } = useGetComponentsInfo();
+    const pageInfo = useGetPageInfo();
+
+    const { loading, run: save } = useRequest(async () => {
+        if (!id) return;
+        await updateQuestionService(id, { ...pageInfo, componentList });
+    }, {
+        manual: true
+    });
+
+    //快捷键
+    useKeyPress(['ctrl.s', 'meta.s'], (event: KeyboardEvent) => {
+        event.preventDefault();      //阻止冒泡，从而不会触发网页的保存，而仅触发问卷相关信息的保存
+        if (!loading) save();
+    })
+
+    //自动保存
+    useDebounceEffect(() => {
+        save()
+    }, [componentList, pageInfo],
+        {
+            wait: 1000
+        });
+
+    return <Button
+        onClick={save}
+        disabled={loading}
+        icon={loading ? <LoadingOutlined /> : null}>
+        保存
+    </Button>
+}
+
+//发布按钮
+const PublishButton: FC = () => {
+    const nav = useNavigate();
+    const { id } = useParams();
+    const { componentList = [] } = useGetComponentsInfo();
+    const pageInfo = useGetPageInfo();
+
+    const { loading, run: pub } = useRequest(async () => {
+        if (!id) return;
+        await updateQuestionService(id, {
+            ...pageInfo,
+            componentList,
+            isPublished: true         //标志着问卷已经被发布
+        });
+    }, {
+        manual: true,
+        onSuccess: () => {
+            message.success('发布成功');
+            nav(`/question/stat/${id}`);       //发布成功，跳转到统计页面
+        }
+    })
+
+    return (
+        <Button
+            type="primary"
+            onClick={pub}
+            disabled={loading}>发布</Button>
+    )
+}
+
+//编辑器头部
 const EditHeader: FC = () => {
     const nav = useNavigate();
 
@@ -20,7 +126,7 @@ const EditHeader: FC = () => {
                             icon={<LeftOutlined />}
                             onClick={() => nav(-1)}
                         >返回</Button>
-                        <Title>问卷标题</Title>
+                        <TitleElem />
                     </Space>
                 </div>
                 <div className={styles.main}>
@@ -28,8 +134,8 @@ const EditHeader: FC = () => {
                 </div>
                 <div className={styles.right}>
                     <Space>
-                        <Button>保存</Button>
-                        <Button type="primary">发布</Button>
+                        <SaveButton />
+                        <PublishButton />
                     </Space>
                 </div>
             </div>
